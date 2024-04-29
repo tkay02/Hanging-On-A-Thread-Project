4561e71b0a633f4afb29490d75a6f21d86ba214c
Thomas Kay <takay1@catamount.wcu.edu>
2024-04-29 13:38:47 -0400

    Added concept for Depot struct

---
+++ project4/src/depot.rs
@@ -0,0 +1,49 @@
+use crate::steward::RESOURCES;
+
+pub struct Depot {
+    burnstone: String,
+    seaplum: String,
+    kleh: String
+}
+
+impl Depot {
+    
+    pub fn new() -> Depot {
+        Depot {
+            burnstone: String::new(),
+            seaplum: String::new(),
+            kleh: String::new()
+        }
+    }
+
+    pub fn place_burnstone(&mut self) {
+        self.burnstone = String::from(RESOURCES[0]);
+    }
+
+    pub fn take_burnstone(&mut self) -> String {
+        let burnstone = self.burnstone.clone();
+        self.burnstone = String::new();
+        burnstone
+    }
+
+    pub fn place_seaplum(&mut self) {
+        self.seaplum = String::from(RESOURCES[1]);
+    }
+
+    pub fn take_seaplum(&mut self) -> String {
+        let seaplum = self.seaplum.clone();
+        self.seaplum = String::new();
+        seaplum
+    }
+
+    pub fn place_kleh(&mut self) {
+        self.kleh = String::from(RESOURCES[2]);
+    }
+
+    pub fn take_kleh(&mut self) -> String {
+        let kleh = self.kleh.clone();
+        self.kleh = String::new();
+        kleh
+    }
+
+}
