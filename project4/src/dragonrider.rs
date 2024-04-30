e11c117e89e30228f736646cde2a7a34e611fc49
Thomas Kay <takay1@catamount.wcu.edu>
2024-04-30 13:10:09 -0400

    Basic framework for dragonrider

---
+++ project4/src/dragonrider.rs
@@ -0,0 +1,25 @@
+use std::sync::{Arc, Condvar, Mutex};
+
+use crate::depot::Depot;
+
+
+
+pub struct DragonRider {
+    main_resource: String,
+    depot: Arc<Mutex<Depot>>,
+    alert: Condvar
+}
+
+impl DragonRider {
+
+    pub fn new(resource:String, depot:Arc<Mutex<Depot>>) -> DragonRider {
+        DragonRider {
+            main_resource: resource,
+            depot: depot,
+            alert: Condvar::new()
+        }
+    }
+
+
+
+}
